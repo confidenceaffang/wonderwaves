65cbb0aaaad9f4e45c78feb299ad0f9ab5ef72d1
confidenceaffang <coaffa01@wsc.edu>
2024-12-21 19:06:45 -0600

    remade the pproject

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Body from "@/Components/Body";
 import Navbar from "@/Components/Navbar";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <section>
-      <Navbar />
-      <Body />
-    </section>
+  <section>
+    <Navbar />
+    <Body />
+  </section>
   );
 }
